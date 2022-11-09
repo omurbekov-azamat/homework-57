6aa90c5d86d65be6dca79c8207fd5a64266be28c
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-09 15:30:22 +0600

    commit-3; created components with some words and added in app.tsx

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserItem from "./UserItem";
+
+const Users = () => {
+  return (
+    <div>
+      Users
+      <UserItem/>
+      <UserItem/>
+      <UserItem/>
+    </div>
+  );
+};
+
+export default Users;
---
+++ src/container/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import UserForm from "../components/UserForm/UserForm";
+import Users from "../components/Users/Users";
+
+function App() {
+  return (
+    <>
+      <main className='container-fluid'>
+        <div className='row'>
+          <div className='col'>
+            <UserForm/>
+          </div>
+          <div className='col'>
+            <Users/>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
