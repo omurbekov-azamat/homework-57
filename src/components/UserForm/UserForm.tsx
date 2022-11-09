cdca8b46be39dec66e98124eb2d2af234e459efb
omurbekov-azamat <avgusttin8@gmail.com>
2022-11-09 16:26:38 +0600

    commit-6; added some function for create new user;

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {User} from "../../types";
 
-const UserForm = () => {
+interface Props {
+  onSubmit: (user: User) => void;
+}
+
+const UserForm:React.FC<Props> = ({onSubmit}) => {
+  const [user, setUser] = useState<User>({
+    name: '',
+    email: '',
+    condition: '',
+    part: '',
+  });
+
+  const onDishChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+
+    setUser(prev => ({...prev, [name]: value}));
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(user)
+    onSubmit({
+    id: Math.random().toString(),
+      ...user,
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
       <h4 className='text-uppercase fw-semibold'>add new user</h4>
       <div className='form-group mt-2'>
         <label htmlFor="name">Name</label>
-        <input id='name' name='name' className='form-control' type="text"/>
+        <input id='name' name='name' type="text"
+               className='form-control'
+               value={user.name}
+               onChange={onDishChange}
+        />
       </div>
       <div className='form-group mt-2'>
         <label htmlFor="email">Email</label>
-        <input id='email' name='email' className='form-control' type="email"/>
+        <input id='email' name='email' type="email"
+               className='form-control'
+               value={user.email}
+               onChange={onDishChange}
+        />
       </div>
       <div className='form-group mt-2'>
         <label htmlFor="condition" className='me-2'>Condition</label>
-        <input id='condition' name='condition' type="checkbox"/>
+        <input id='condition' name='condition' type="checkbox"
+               value={user.condition}
+               onChange={onDishChange}
+        />
       </div>
       <div className='form-group mt-2'>
         <label htmlFor="part" className='me-2'>Part</label>
         <select name="part" id="part" className='form-control'>
-          <option value="user.value">user</option>
-          <option value="editor.value">editor</option>
-          <option value="admin">admin</option>
+          <option value={user.part}>user</option>
+          <option value={user.part}>editor</option>
+          <option value={user.part}>admin</option>
         </select>
       </div>
       <button type='submit' className='btn btn-info mt-3'>Create</button>
--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserForm from "../components/UserForm/UserForm";
 import Users from "../components/Users/Users";
+import {User} from "../types";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const addUser = (newUser: User) => {
+    setUsers(prev => [...prev, newUser]);
+  };
+
   return (
     <>
       <main className='container'>
         <div className='row'>
           <div className='col'>
-            <UserForm/>
+            <UserForm onSubmit={addUser}/>
           </div>
           <div className='col'>
             <Users/>
